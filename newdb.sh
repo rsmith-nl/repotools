1a4d1d56f2c7dee184da07d5c2e90b586691ed3d
Roland Smith <rsmith@xs4all.nl>
2022-11-07 23:42:12 +0100

    Call makedb.py from newdb.sh.

--- newdb.sh
+++ newdb.sh
@@ -5,7 +5,7 @@
 # Copyright © 2022 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2022-11-06T11:02:30+0100
-# Last modified: 2022-11-06T11:46:24+0100
+# Last modified: 2022-11-07T23:41:02+0100
 
 curl --silent http://pkg.freebsd.org/freebsd:13:x86:64/quarterly/packagesite.txz | \
 tar -xOf - packagesite.yaml > new.yaml
@@ -13,11 +13,12 @@ tar -xOf - packagesite.yaml > new.yaml
 diff -q packagesite.yaml new.yaml >/dev/null
 DIFFRESULT=$?
 if [ $DIFFRESULT -eq 1 ]; then
-    echo "Package database has been updated"
     NEWNAME=`ls -l -D '%Y%m%d' packagesite.yaml | awk '{printf "%s-%s", $6, $7}'`
     mv -f packagesite.yaml $NEWNAME
     mv new.yaml packagesite.yaml
+    echo "Package database has been updated"
     chmod 400 packagesite.yaml
+    ./makedb.py
 elif [ $DIFFRESULT -gt 1 ]; then
     echo "An error occurred. diff returned $DIFFRESULT"
     rm -f new.yaml
