3ce2f345ec517284f19bbd6296b3c082cca7d336
Roland Smith <rsmith@xs4all.nl>
2023-12-09 22:10:31 +0100

    Update newdb for FreeBSD 14.

--- newdb.sh
+++ newdb.sh
@@ -5,10 +5,12 @@
 # Copyright © 2022 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2022-11-06T11:02:30+0100
-# Last modified: 2023-07-30T09:49:33+0200
+# Last modified: 2023-12-09T22:09:17+0100
 
-curl --silent http://pkg.freebsd.org/freebsd:13:x86:64/quarterly/packagesite.txz | \
+echo -n "Downloading new package database... "
+curl --silent http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/packagesite.txz | \
 tar -xOf - packagesite.yaml > new.yaml
+echo "done."
 
 diff -q packagesite.yaml new.yaml >/dev/null
 DIFFRESULT=$?
@@ -16,18 +18,18 @@ if [ $DIFFRESULT -eq 1 ]; then
     NEWNAME=`ls -l -D '%Y%m%d' packagesite.yaml | awk '{printf "%s-%s", $6, $7}'`
     mv -f packagesite.yaml $NEWNAME
     mv new.yaml packagesite.yaml
-    curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:13:x86:64/quarterly/packagesite.txz
-    curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:13:x86:64/quarterly/meta.conf
-    curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:13:x86:64/quarterly/meta.txz
-
-    echo "Package database has been updated"
+    echo -n "Updating package files... "
+    curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/packagesite.txz
+    curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/meta.conf
+    curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/meta.txz
+    echo "done."
     chmod 400 packagesite.yaml
     ./makedb
 elif [ $DIFFRESULT -gt 1 ]; then
     echo "An error occurred. diff returned $DIFFRESULT"
     rm -f new.yaml
 else
-    echo "No changes in package database"
+    echo "No changes in package database."
     rm -f new.yaml
 fi
 
