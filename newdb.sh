4754688339f747d2f2d7e9cd03cce0009ee99670
Roland Smith <rsmith@xs4all.nl>
2023-12-11 20:55:25 +0100

    Also download pkg file.

--- newdb.sh
+++ newdb.sh
@@ -5,7 +5,7 @@
 # Copyright © 2022 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2022-11-06T11:02:30+0100
-# Last modified: 2023-12-11T20:35:06+0100
+# Last modified: 2023-12-11T20:54:47+0100
 
 echo -n "Downloading new package database... "
 curl --silent http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/packagesite.txz | \
@@ -20,10 +20,10 @@ if [ $DIFFRESULT -eq 1 ]; then
     mv new.yaml packagesite.yaml
     echo -n "Updating package files... "
     curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/packagesite.txz
+    curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/packagesite.pkg
     curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/meta.txz
     echo "done."
     chmod 400 packagesite.yaml
-    ln -sf repo/packagesite.txz repo/packagesite.pkg
     ./makedb
 elif [ $DIFFRESULT -gt 1 ]; then
     echo "An error occurred. diff returned $DIFFRESULT"
