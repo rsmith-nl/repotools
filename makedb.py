f955dc611154865a0158dbeadd38912b006cba36
Roland Smith <rsmith@xs4all.nl>
2024-08-16 16:13:51 +0200

    Use main.

--- makedb.py
+++ makedb.py
@@ -5,7 +5,7 @@
 # Copyright © 2022 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2022-10-10T23:13:41+0200
-# Last modified: 2024-08-16T16:11:43+0200
+# Last modified: 2024-08-16T16:13:28+0200
 
 import glob
 import hashlib
@@ -93,100 +93,101 @@ def insert_pkg(cur, pkg):
 
 
 # Main program starts here
-start = time.monotonic()
-print("Loading package info from yaml file... ", end="")
-with open("packagesite.yaml") as yf:
-    lines = [ln.strip() for ln in yf.readlines()]
-jsondata = "[" + ", ".join(lines) + "]"
-packages = json.loads(jsondata)
-print("done")
-print(f"Found {len(packages)} packages.")
+if __name__ == "__main__":
+    start = time.monotonic()
+    print("Loading package info from yaml file... ", end="")
+    with open("packagesite.yaml") as yf:
+        lines = [ln.strip() for ln in yf.readlines()]
+    jsondata = "[" + ", ".join(lines) + "]"
+    packages = json.loads(jsondata)
+    print("done")
+    print(f"Found {len(packages)} packages.")
 
-# Remove existing database
-if os.path.exists("packagesite.db"):
-    os.remove("packagesite.db")
-    print("Old database removed.")
+    # Remove existing database
+    if os.path.exists("packagesite.db"):
+        os.remove("packagesite.db")
+        print("Old database removed.")
 
-# Create database
-db = sqlite3.connect("packagesite.db")
-print("Database created")
-cur = db.cursor()
+    # Create database
+    db = sqlite3.connect("packagesite.db")
+    print("Database created")
+    cur = db.cursor()
 
-# Create tables
-tbls = """CREATE TABLE packages (name TEXT, origin TEXT,
-version TEXT, comment TEXT, maintainer TEXT, www TEXT, abi TEXT, arch TEXT,
-prefix TEXT, sum TEXT, flatsize INT, path TEXT, repopath TEXT,
-licenselogic TEXT, pkgsize INT, desc TEXT);
-CREATE TABLE licenses (pkgid INT, name txt);
-CREATE TABLE deps (pkgid INT, name TEXT, origin TEXT, version TEXT, depid INT);
-CREATE TABLE categories (pkgid INT, name txt);
-CREATE TABLE shlibs_required (pkgid INT, name txt);
-CREATE TABLE shlibs_provided (pkgid INT, name txt);
-CREATE TABLE options (pkgid INT, key TEXT, value TEXT);
-CREATE TABLE annotations (pkgid INT, key TEXT, value TEXT);
-"""
-cur.executescript(tbls)
-db.commit()
-print("Tables created.")
+    # Create tables
+    tbls = """CREATE TABLE packages (name TEXT, origin TEXT,
+    version TEXT, comment TEXT, maintainer TEXT, www TEXT, abi TEXT, arch TEXT,
+    prefix TEXT, sum TEXT, flatsize INT, path TEXT, repopath TEXT,
+    licenselogic TEXT, pkgsize INT, desc TEXT);
+    CREATE TABLE licenses (pkgid INT, name txt);
+    CREATE TABLE deps (pkgid INT, name TEXT, origin TEXT, version TEXT, depid INT);
+    CREATE TABLE categories (pkgid INT, name txt);
+    CREATE TABLE shlibs_required (pkgid INT, name txt);
+    CREATE TABLE shlibs_provided (pkgid INT, name txt);
+    CREATE TABLE options (pkgid INT, key TEXT, value TEXT);
+    CREATE TABLE annotations (pkgid INT, key TEXT, value TEXT);
+    """
+    cur.executescript(tbls)
+    db.commit()
+    print("Tables created.")
 
-print("Inserting data into tables... ", end="")
-for pkg in packages:  # noqa
-    insert_pkg(cur, pkg)
-db.commit()
-print("done.")
+    print("Inserting data into tables... ", end="")
+    for pkg in packages:  # noqa
+        insert_pkg(cur, pkg)
+    db.commit()
+    print("done.")
 
-# Insert packages from repo that are not in the database,
-# and fix size/checksum mismatches
-print("Inserting packages not in database... ")
-for completename in glob.glob(PKGDIR + "*.pkg"):
-    repopath = completename.removeprefix(REPO)
-    cursize = os.path.getsize(completename)
-    with open(completename, "rb") as filecontents:
-        data = filecontents.read()
-        cursum = hashlib.sha256(data).hexdigest()
-        del data
-    pkgname = completename[9:-4].rsplit("-", maxsplit=1)[0]
-    try:
-        pkgid, dbsum, dbsize = cur.execute(
-            "SELECT rowid, sum, pkgsize FROM packages WHERE name==?", (pkgname,)
-        ).fetchone()
-    except (ValueError, TypeError):
-        print(f"# adding {pkgname} to database... ", end="")
+    # Insert packages from repo that are not in the database,
+    # and fix size/checksum mismatches
+    print("Inserting packages not in database... ")
+    for completename in glob.glob(PKGDIR + "*.pkg"):
+        repopath = completename.removeprefix(REPO)
+        cursize = os.path.getsize(completename)
+        with open(completename, "rb") as filecontents:
+            data = filecontents.read()
+            cursum = hashlib.sha256(data).hexdigest()
+            del data
+        pkgname = completename[9:-4].rsplit("-", maxsplit=1)[0]
         try:
-            manifest = get_manifest(completename)
-            # Add missing data.
-            manifest["sum"] = cursum
-            manifest["repopath"] = repopath
-            manifest["path"] = repopath
-            manifest["pkgsize"] = cursize
-            insert_pkg(cur, manifest)
-        except ValueError:
-            print(f"# skipping {pkgname}, could not get manifest")
-        print("done")
-        continue
-    if dbsum != cursum or dbsize != cursize:
-        print(f"# updating {pkgname}... ", end="")
-        cur.execute(
-            "UPDATE packages SET sum = ?, pkgsize = ? WHERE rowid == ?",
-            (cursum, cursize, pkgid),
-        )
-        print("done")
-db.commit()
-
-# Only after all packages have been ID'd can we resolve deps.
-print("Resolving dependencies... ", end="")
-idbyname = dict(cur.execute("SELECT name, rowid FROM packages").fetchall())
-for pkg in packages:
-    if "deps" in pkg:
-        for depname, depdata in pkg["deps"].items():
-            deporig, depver = depdata.values()
-            depid = idbyname.get(depname, -1)
+            pkgid, dbsum, dbsize = cur.execute(
+                "SELECT rowid, sum, pkgsize FROM packages WHERE name==?", (pkgname,)
+            ).fetchone()
+        except (ValueError, TypeError):
+            print(f"# adding {pkgname} to database... ", end="")
+            try:
+                manifest = get_manifest(completename)
+                # Add missing data.
+                manifest["sum"] = cursum
+                manifest["repopath"] = repopath
+                manifest["path"] = repopath
+                manifest["pkgsize"] = cursize
+                insert_pkg(cur, manifest)
+            except ValueError:
+                print(f"# skipping {pkgname}, could not get manifest")
+            print("done")
+            continue
+        if dbsum != cursum or dbsize != cursize:
+            print(f"# updating {pkgname}... ", end="")
             cur.execute(
-                "INSERT INTO deps VALUES (?, ?, ?, ?, ?)",
-                (idbyname[pkg["name"]], depname, deporig, depver, depid),
+                "UPDATE packages SET sum = ?, pkgsize = ? WHERE rowid == ?",
+                (cursum, cursize, pkgid),
             )
-print("done.")
-db.commit()
-db.close()
-runtime = time.monotonic() - start
-print(f"# duration: {runtime:.3f} s")
+            print("done")
+    db.commit()
+
+    # Only after all packages have been ID'd can we resolve deps.
+    print("Resolving dependencies... ", end="")
+    idbyname = dict(cur.execute("SELECT name, rowid FROM packages").fetchall())
+    for pkg in packages:
+        if "deps" in pkg:
+            for depname, depdata in pkg["deps"].items():
+                deporig, depver = depdata.values()
+                depid = idbyname.get(depname, -1)
+                cur.execute(
+                    "INSERT INTO deps VALUES (?, ?, ?, ?, ?)",
+                    (idbyname[pkg["name"]], depname, deporig, depver, depid),
+                )
+    print("done.")
+    db.commit()
+    db.close()
+    runtime = time.monotonic() - start
+    print(f"# duration: {runtime:.3f} s")
