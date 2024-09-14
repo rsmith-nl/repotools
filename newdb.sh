58225159f150570afa579156bd3578348db6b737
Roland Smith <rsmith@xs4all.nl>
2024-09-14 11:34:27 +0200

    Further colorize newdb output.

--- newdb.sh
+++ newdb.sh
@@ -5,7 +5,7 @@
 # Copyright © 2022 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2022-11-06T11:02:30+0100
-# Last modified: 2024-09-14T11:09:31+0200
+# Last modified: 2024-09-14T11:33:44+0200
 
 BOLD_WHITE='\033[1;37m'
 CYAN='\033[0;36m'
@@ -19,30 +19,28 @@ RESET='\033[0m' # No Color
 echo -n "Downloading new package database... "
 curl --silent -O http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/packagesite.txz
 tar -xOf packagesite.txz packagesite.yaml > new.yaml
-echo "${GREEN}done.${RESET}"
+printf "${GREEN}done.${RESET}\n"
 
-echo "Verifying public key..."
+echo -n "Verifying public key... "
 tar -xOf packagesite.txz packagesite.yaml.pub > new.pub
-openssl pkey -pubcheck -pubin -in new.pub -noout
-CHECKRESULT=$?
-if [ $CHECKRESULT -ne 0 ]; then
-    echo "${BOLD_RED}An error occurred. Public key is not valid. Exiting!${RESET}"
+KEYOUT=$(openssl pkey -pubcheck -pubin -in new.pub -noout)
+if [ "${KEYOUT}" != "Key is valid" ]; then
+    printf "\n${BOLD_RED}An error occurred. Public key is not valid. Exiting!${RESET}\n"
     rm -f new.yaml new.pub
     exit 1
 else
-    echo "${GREEN}done.${RESET}"
+    printf "${GREEN}${KEYOUT}${RESET}\n"
 fi
-echo "Verifying digest..."
+echo -n "Verifying digest... "
 tar -xOf packagesite.txz packagesite.yaml.sig > new.sig
-sha256 -q new.yaml | tr -d '\n' | \
-    openssl dgst -verify new.pub -signature new.sig
-CHECKRESULT=$?
-if [ $CHECKRESULT -ne 0 ]; then
-    echo "${BOLD_RED}An error occurred. Verification failed. Exiting!${RESET}"
+DGSTOUT=$(sha256 -q new.yaml | tr -d '\n' | \
+    openssl dgst -verify new.pub -signature new.sig)
+if [ "${DGSTOUT}" != "Verified OK" ]; then
+    printf "\n${BOLD_RED}An error occurred. Verification failed. Exiting!${RESET}\n"
     rm -f new.yaml new.pub new.sig
     exit 1
 else
-    echo "${GREEN}done.${RESET}"
+    printf "${GREEN}${DGSTOUT}${RESET}\n"
 fi
 rm -f new.pub new.sig
 
@@ -58,14 +56,14 @@ if [ $DIFFRESULT -eq 1 ]; then
     curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/data.txz
     cp -p repo/data.txz repo/data.pkg
     curl --silent -O --output-dir repo http://pkg.freebsd.org/freebsd:14:x86:64/quarterly/meta.txz
-    echo "${GREEN}done.${RESET}"
+    printf "${GREEN}done.${RESET}\n"
     chmod 400 packagesite.yaml
     ./makedb
 elif [ $DIFFRESULT -gt 1 ]; then
-    echo "${BOLD_RED}An error occurred. diff returned $DIFFRESULT${RESET}"
+    printf "${BOLD_RED}An error occurred. diff returned $DIFFRESULT${RESET}\n"
     rm -f new.yaml
 else
-    echo "${CYAN}No changes in package database.${RESET}"
+    printf "${CYAN}No changes in package database.${RESET}\n"
     rm -f new.yaml
 fi
 
